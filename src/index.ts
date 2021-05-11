7ca2216e86f0390e249b7bf352ae574941d20448
adam-lafontaine <adam.lafontaine@gmail.com>
2021-05-10 21:36:49 -0400

    servie client

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors" // https://expressjs.com/en/resources/middleware/cors.html
-// import path from "path"
+import path from "path"
 import { HomeController } from "./controllers/home_controller";
 
 
-// const cors_options = { origin: ["http://localhost:8081"]};
+// const cors_options = { origin: ["http://localhost:8080"]};
 const cors_all = cors();
 
 const app = express();
@@ -18,9 +18,11 @@ const port = 8081; // default port to listen
 const controller = new HomeController();
 
 // define routes
-app.get("/", controller.home);
-app.get("/props", controller.request_props);
-app.get("/:id", controller.try_echo);
+app.get("/", (req, res) =>{ res.sendFile(path.join(__dirname, "client", "index.html"));})
+
+app.get("/api", controller.home);
+app.get("/api/props", controller.request_props);
+app.get("/api/:id", controller.try_echo);
 
 app.post("/update", controller.update_data);
 
